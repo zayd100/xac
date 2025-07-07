da497f79910685771a100e449e2c377c394355c2
zaid <zaidshabbir666@gmail.com>
2025-07-07 15:39:32 -0700

    added images to cards components

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,10 +1,14 @@
 import '../styles/card.css'
-const Card = ({name,type,price})=>{
+const Card = ({name,type,price,image})=>{
     return(
+
                 <div className="card">
+                    <div className='card-image'>
+                        <img src={image} alt={name} />
+                    </div>
                     <h1 className='card-title'>{name}</h1>
                     <h2 className='card-type'>{type}</h2>
-                    <p>{price}</p>
+                    <p>{price}$</p>
                 </div>
 
     );
--- src/pages/rent.jsx
+++ src/pages/rent.jsx
@@ -1,23 +1,26 @@
 import Card from "../components/card";
-
+import sofa from "../assets/sofa.png";
+import chair from "../assets/chair.png";
 const Rent= () =>{
     const carddata = [
 {
     name: "sofa",
     type:"luxury",
-    price:231
+    price:1200,
+    image: sofa
 },
 {
-    name: "sofa",
+    name: "chair",
     type:"luxury",
-    price:231
+    price:300,
+    image: chair
 }
     ];
     return(
         <div >
         <p>Rent</p>
         {carddata.map((item, index) => (
-            <Card key={index} name={item.name} type={item.type} price={item.price} />
+            <Card key={index} name={item.name} type={item.type} price={item.price} image={item.image}/>
         ))}
         </div>
     );
