e3629e761879299af2f0607184e22186e3f67a6c
zaid <zaidshabbir666@gmail.com>
2025-07-07 13:31:13 -0700

    added navbar and styling.

---
+++ src/components/navbar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import '../styles/navbar.css';
+function Navbar(){
+    return(
+        <nav className="navbar">
+            <ul>Home</ul>
+            <ul>Rent</ul>
+            <ul>About Us</ul>
+        </nav>
+    );
+}
+export default Navbar;
